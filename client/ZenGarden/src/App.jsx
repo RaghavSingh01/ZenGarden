3135d38cf150db0616bf4642cc1123562e38e6de
Raghav Singh <dhankarraghav13@gmail.com>
2025-09-22 21:30:10 +0530

    vercel speed insights

--- client/ZenGarden/src/App.jsx
+++ client/ZenGarden/src/App.jsx
@@ -5,6 +5,7 @@
 import React from 'react';
 import Router from './routes/Router.jsx';
 import ToastProvider from './components/UI/ToastProvider.jsx';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
   return(
